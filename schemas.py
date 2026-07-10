ca8792aac61f231282b068e392acd5573a7ed140
Pratik Suchak <you@example.com>
2026-07-10 08:47:48 +0530

    Authorization feature

--- schemas.py
+++ schemas.py
@@ -42,8 +42,7 @@ class PostBase(BaseModel):
 
 
 class PostCreate(PostBase):
-    user_id: int  # TEMPORARY
-
+    pass
 
 class PostUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=1, max_length=100)
